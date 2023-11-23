cd0a699af9b65d943e73fbcfd814c68e290ec95a
ipz06 <it.vania.9@gmail.com>
2023-11-23 15:25:35 +0200

    Add style

--- my-portfolio/src/app/voice/page.tsx
+++ my-portfolio/src/app/voice/page.tsx
@@ -5,6 +5,8 @@ export default function Voice () {
     
     return (
         <>
+
+            <h2 className="text-xl font-bold tracking-tight text-slate-200 sm:text-lg py-1">TEAMMATE`S VOICE</h2>
             <ul id='voice' >
                 {feedback.map(mate => <li key={mate.id} className={styles.card}> {mate.text} <h3 className="font-bold">{mate.name} </h3><span className="italic">{mate.mate}</span></li>)}
             </ul>
