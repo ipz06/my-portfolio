3740de1e8730e378b09a967d3fbdfc76d29ee5ee
ipz06 <it.vania.9@gmail.com>
2023-11-25 13:20:11 +0200

    Add feedback

--- my-portfolio/src/app/voice/page.tsx
+++ my-portfolio/src/app/voice/page.tsx
@@ -4,13 +4,13 @@ import styles from '../ui/card.module.css';
 export default function Voice () {
     
     return (
-        <>
+        <div id='voice'>
 
-            <h2 className="text-xl font-bold tracking-tight text-slate-200 sm:text-lg py-1">TEAMMATE`S VOICE</h2>
-            <ul id='voice' >
-                {feedback.map(mate => <li key={mate.id} className={styles.card}> {mate.text} <h3 className="font-bold">{mate.name} </h3><span className="italic">{mate.mate}</span></li>)}
+            <h2 className="text-3xl font-bold tracking-tight text-[#3BBA9C] py-10">TEAMMATE`S VOICE</h2>
+            <ul>
+                {feedback.map(mate => <li key={mate.id} className={styles.card}> {mate.text} <h3 className="font-bold text-[#d6d6d6] py-5">{mate.name} </h3><span className="italic text-[#d6d6d6]">{mate.mate}</span></li>)}
             </ul>
             
-        </>
+        </div>
     )
 }
