21b49c8490ce0f69eb4297ec0b0fba3fd62ff56c
ipz06 <it.vania.9@gmail.com>
2023-11-25 13:22:32 +0200

    Add say hello

--- my-portfolio/src/app/sayHello/page.tsx
+++ my-portfolio/src/app/sayHello/page.tsx
@@ -1,8 +1,9 @@
 import { SubmitButton } from "./submitButton"
 import { MdWavingHand } from "react-icons/md";
+import { motion } from "framer-motion";
 
 
-export default function SayHello () {
+export default function Page () {
 
     // const formSubmissionHandler = () => {
 
@@ -15,15 +16,20 @@ export default function SayHello () {
         // mutate data
         // revalidate cache
 
-        
+
       }
 
     return (
         <div className="flex-1 flex-col justify-center items-center px-96 ml-28 my-44">
-            <div className="text-slate-800 m-10 px-40">
-            <MdWavingHand size={90} />
-            </div>
-            
+            {/* <motion.div className="text-slate-800 m-10 px-40" 
+            whileHover={{ scale: 1.2, rotate: 90 }}
+            whileTap={{ scale: 0.8, rotate: -90, borderRadius: "100%" }}>
+                <MdWavingHand size={90} />
+            </motion.div>
+             */}
+             <motion.div animate={{ opacity: 1 }} initial={{ opacity: 0 }}>
+                Hello, Framer Motion!
+            </motion.div>
         <form action={create} method="post" className=" m-3 w-full">
             <div className="m-3">
                 <label htmlFor="name">Enter your name: </label>
