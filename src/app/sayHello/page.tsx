df59298ce954cba818a6a9daeda4da5f9c439358
ipz06 <it.vania.9@gmail.com>
2023-11-29 13:15:40 +0200

    Change form - add useForm hook and styles

--- src/app/sayHello/page.tsx
+++ src/app/sayHello/page.tsx
@@ -1,25 +1,28 @@
+'use client'
+
+import { SyntheticEvent, useRef, useState } from "react";
 import { SubmitButton } from "./submitButton"
+import {SubmitHandler, useForm} from 'react-hook-form';
+import { MdWavingHand } from "react-icons/md";
+import './form.css';
 
-
+type FormValues = {
+    name: string,
+    email: string,
+    story: string
+  }
+  
 
 export default function Page () {
+    const {register, handleSubmit} = useForm<FormValues>();
 
-    // const formSubmissionHandler = () => {
+    const onSubmit:SubmitHandler<FormValues> = (data) => {
+        alert(JSON.stringify(data));
+    }
 
-    // }
-
-    
-    // async function create(formData: FormData) {
-    //     'use server'
-     
-    //     // mutate data
-    //     // revalidate cache
-
-
-    //   }
 
     return (
-        <div className="flex-1 flex-col justify-center items-center px-96 ml-28 my-44">
+        <div className="flex flex-col justify-center items-center max-w-4xl w-full px-10 pt-6 pb-8 shadow-sm bg-slate-100">
             {/* <motion.div className="text-slate-800 m-10 px-40" 
             whileHover={{ scale: 1.2, rotate: 90 }}
             whileTap={{ scale: 0.8, rotate: -90, borderRadius: "100%" }}>
@@ -30,21 +33,42 @@ export default function Page () {
                 Hello, Framer Motion!
             </motion.div> */}
         {/* <form action={create} method="post" className=" m-3 w-full"> */}
-        <form className=" m-3 w-full"> 
-            <div className="m-3">
+        <div className="text-center text-4xl font-semibold bg-clip-text ">Contact me</div>
+        <form className="m-3 w-full pt-8 " onSubmit={handleSubmit(onSubmit)}> 
+            <div className="flex flex-col mx-5 mt-8 h-10 relative ">
                 <label htmlFor="name">Enter your name: </label>
-                <input type="text" name="name" placeholder='Name' className='border-2 border-r-2' required />
+                <input 
+                type="text" 
+                placeholder='Name' 
+                className='border-b-4 bg-inherit'
+                {...register("name")} 
+                // ref={name}
+                required />
             </div>
-            <div className="m-3">
+            <div className="flex flex-col mx-5 mt-8 h-10 relative">
                 <label htmlFor="email">Enter your email: </label>
-                <input type="email" name="email" id="email" placeholder="Email" className='border-2 border-r-2' required />
+                <input 
+                type="email"  
+                {...register('email')}
+                // ref={email}
+                placeholder="Email" 
+                className='border-2 border-r-2' 
+                required />
             </div>
-            <div className="m-3">
+            <div className="flex flex-col mx-5 mt-8 h-20 relative">
                 <label htmlFor="text">Enter your message: </label>
-                <textarea id="story" name="story"  placeholder="Message" className='border-2 border-r-2' required />
+                <textarea 
+                id="story"
+                {...register('story')}  
+                // ref={text}
+                placeholder="Message" 
+                className='border-2 border-r-2' 
+                required />
             </div>
-            <div className='m-10 px-24' >
-                <SubmitButton/>
+            <div className='flex flex-col justify-start items-start m-3 my-10' >
+                {/* <SubmitButton onSend={handleSend}/> */}
+                {/* <button type="button" className="button" onClick={handleSend}>Send</button> */}
+                <input type='submit' value='submit'/>
             </div>      
         </form>
         </div>
--- src/app/sayHello/submitButton.tsx
+++ src/app/sayHello/submitButton.tsx
@@ -1,12 +1,16 @@
 'use client'
- 
+
+type Props = {
+  onSend: MouseEventHandler<HTMLButtonElement>
+}
+import { MouseEventHandler } from 'react'
 import { useFormStatus } from 'react-dom'
  
-export function SubmitButton() {
+export function SubmitButton({onSend}: Props) {
   const { pending } = useFormStatus()
  
   return (
-    <button type="submit" aria-disabled={pending} className='border-2 px-10'>
+    <button type="button" aria-disabled={pending} className='border-2 px-10' onClick={onSend}>
       Send
     </button>
   )
