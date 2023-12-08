2601ea979003bd3d7276fa1de3702282b85a3c97
ipz06 <it.vania.9@gmail.com>
2023-12-08 13:16:42 +0200

    Add effect on scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,31 +12,133 @@ import Link from 'next/link';
 import Projects from './projects/page';
 import { TbTicTac } from "react-icons/tb";
 import { BiMessageDetail } from "react-icons/bi";
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import 'react-tooltip/dist/react-tooltip.css';
 
 import { Tooltip } from 'react-tooltip';
+import { useIsVisible } from './hooks/useIsVisible';
 
 
 
 
 export default function Home() {
-  const [isHovering, setIsHovering] = useState(false);
 
-  const handleMouseOver = () => {
-    setIsHovering(true);
-  };
+  const sections = [
+    'about',
+    'knowledge',
+    'projects',
+    'education',
+    'testimonials',
+    'contact',
+  ];
 
-  const handleMouseOut = () => {
-    setIsHovering(false);
-  };
+  useEffect(() => {
+  
+    const observer = new IntersectionObserver(entries => {
+
+      let intersectionFound = false;
+
+      
+
+      entries.forEach(entry => {
+        if (intersectionFound) return;
+
+        
+        const currentSection = sections.find(section => entry.target.id === section);
+        const navElement = document.getElementById(`#${currentSection}-nav`);
+
+        if (entry.isIntersecting) {
+          console.log(`nav: ${currentSection}`);
+          navElement?.classList.add(
+            'group',
+            'flex',
+            'items-center',
+            'py-3',
+            'active',
+            'nav-indicator',
+            'h-px',
+            'transition-all',
+            'group:w-16',
+            'bg-slate-200',
+            'group-visible:w-16',
+            'group-visible:bg-slate-200',
+            'nav-text',
+            'text-xs',
+            'font-bold',
+            'uppercase',
+            'tracking-widest',
+            'group:text-slate-200',
+            'group-visible:text-slate-200'
+          );
+
+          intersectionFound = true;
+
+        } else {
+          navElement?.classList.remove(
+            'group',
+            'flex',
+            'items-center',
+            'py-3',
+            'active',
+            'nav-indicator',
+            'h-px',
+            'transition-all',
+            'group:w-16',
+            'bg-slate-200',
+            'group-visible:w-16',
+            'group-visible:bg-slate-200',
+            'nav-text',
+            'text-xs',
+            'font-bold',
+            'uppercase',
+            'tracking-widest',
+            'group:text-slate-200',
+            'group-visible:text-slate-200'
+          );
+        }
+      });
+    });
+
+    sections.forEach(sectionId => {
+      console.log('observe', observer)
+      observer.observe(document.getElementById(sectionId));
+    });
+
+    // Clean up the observer on component unmount
+    return () => {
+      // Unobserve each section element
+      sections.forEach(sectionId => {
+        const sectionElement = document.getElementById(sectionId);
+        observer.unobserve(sectionElement);
+      });
+
+      observer.disconnect();
+    };
+  }, []);
 
   return (
-    <div className='flex w-full font-sans min-h-screen mx-auto max-w-screen-xl bg-[#1E1F24] px-6 md:px-12 lg:px-24 lg:py-0 md:py-10'>
+    <div
+    className={`flex w-full font-sans min-h-screen mx-auto max-w-screen-xl bg-[#1E1F24] px-6 md:px-12 lg:px-24 lg:py-0 md:py-10`}
+    // ref={divRef}
+    // ref={ref1}
+    >
+
+    {/* // onMouseMove={handleMouseMove}
+    // onFocus={handleFocus}
+    // onBlur={handleBlur}
+    // onMouseEnter={handleMouseEnter}
+    // onMouseLeave={handleMouseLeave}> */}
+      {/* <div
+        className="pointer-events-none absolute -inset-px opacity-0 transition duration-300 z-10"
+        style={{
+          opacity,
+          background: `radial-gradient(1000px circle at ${position.x}px ${position.y}px, rgba(255,255,255,.02), transparent 50%)`,
+        }}
+      /> */}
       <div className='lg:flex lg:justify-between lg:gap-4'>
         <header className='lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:flex-col lg:justify-between lg:w-1/2 lg:py-12'>
-        <aside>
-          <div className=''>
+        <aside>  
+          <div>
             <Image 
             className='py-10'
             src='/profile.JPG'
@@ -76,7 +178,7 @@ export default function Home() {
             </div>
             <nav className='nav hidden lg:block'>
               <ul className='my-5 w-max'>
-                {navMenu.map(el => <li key={el.ref}><a className='group flex items-center py-3 active' href={el.ref}>
+                {navMenu.map(el => <li key={el.ref} id={`${el.ref}-nav`}><a className='group flex items-center py-3 active' href={el.ref} >
                     <span className='nav-indicator mr-4 h-px w-8 bg-[#707070] transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none' >
                     </span>
                     <span className='nav-text text-xs font-bold uppercase tracking-widest text-[#707070] group-hover:text-slate-200 group-focus-visible:text-slate-200'>
@@ -99,7 +201,7 @@ export default function Home() {
             <span >
               <Link 
               href='/sayHello'
-              className=' hover:text-[#3BBA9C] z-0 relative hover:after:absolute hover:after:top-0' 
+              className=' hover:text-[#3BBA9C] z-0 relative' 
               data-tooltip-id="my-tooltip"
               data-tooltip-content="Write me anything!"
               data-tooltip-place="top"
@@ -117,6 +219,14 @@ export default function Home() {
           <Education />
           <Voice/>
           <Contact />
+          <footer className='max-w-md py-16 px-5 text-sm text-[#707070] sm:pb-0'>
+            <p>
+            {`All rights reserved © 2023 `}
+            </p>
+            <p>
+            Built with Next.js and Tailwind CSS, deployed with Vercel.
+            </p>
+          </footer>
         </main>
       </div>
     </div>
