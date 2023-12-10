fe375ca5f608aba5b8d5c7b7b90f9bf84bb7ac18
ipz06 <it.vania.9@gmail.com>
2023-12-10 23:22:23 +0200

    Try to fic recaptcha

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -3,20 +3,12 @@
 import axios from "axios"
 
 export async function verifyCaptcha(token: string | null) {
-    try 
-    {
-        const res = await axios.post(
-        `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.SITE_SECRET}&response=${token}`
-        )
-        console.log("RESULT:" + res)
-        if (res.data.success) {
-            
-            return "success!"
-        } else {
-            throw new Error("Failed Captcha")
-        }
-    } catch (e) {
-        console.log(e);
-    }
-
+  const res = await axios.post(
+    `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`
+  )
+  if (res.data.success) {
+    return "success!"
+  } else {
+    throw new Error("Failed Captcha")
+  }
 }
