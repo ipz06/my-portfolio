79546545a6368c1a7868b1f83166ef1ec87fe4cf
ipz06 <it.vania.9@gmail.com>
2023-12-15 13:25:57 +0200

    change icon size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -224,7 +224,7 @@ export default function Home() {
                   data-tooltip-place="top"
                   >
                     <Tooltip id="play" arrowColor='#707070' style={{ backgroundColor: "#707070", color: "white" }}/>
-                  <TbTicTac size={50}/>
+                  <TbTicTac size={40}/>
                 </Link> 
               </div>
             </div>
