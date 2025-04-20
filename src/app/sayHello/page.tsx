f546574d88b8ec33b15a306271c9f61654b8a305
ipz06 <it.vania.9@gmail.com>
2025-04-20 23:10:17 +0300

    Remove Recaptcha

--- src/app/sayHello/page.tsx
+++ src/app/sayHello/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import ReCAPTCHA from "react-google-recaptcha"
+// import ReCAPTCHA from "react-google-recaptcha"
 import { verifyCaptcha } from "../actions"
 import { useRef, useState } from "react"
 // import {SubmitHandler, useForm} from 'react-hook-form';
@@ -19,7 +19,7 @@ type FormValues = {
 
 export default function Page () {
 
-    const recaptchaRef = useRef<ReCAPTCHA>(null);
+    // const recaptchaRef = useRef<ReCAPTCHA>(null);
     const [isVerified, setIsVerified] = useState<boolean>(false);
     const router = useRouter();
 
@@ -37,69 +37,12 @@ export default function Page () {
     }
     // const {register, handleSubmit} = useForm<FormValues>();
 
-    async function handleCaptchaSubmission(token: string | null) {
-        // Server function to verify captcha
-        await verifyCaptcha(token)
-          .then(() => setIsVerified(true))
-          .catch(() => setIsVerified(false))
-      };
-
-
-
-    // const {
-    //     value: enteredName,
-    //     isValid: nameIsValid,
-    //     hasError: nameHasError,
-    //     valueChangeHandler: nameChangeHandler,
-    //     inputBlurHandler: nameBlurHandler,
-    //     reset: nameReset,
-    //   } = useInput((value:string):boolean => value.trim() !== '');
-
-    // const {
-    // value: enteredEmail,
-    // isValid: emailIsValid,
-    // hasError: emailHasError,
-    // valueChangeHandler: emailChangeHandler,
-    // inputBlurHandler: emailBlurHandler,
-    // reset: emailReset,
-    // } = useInput((value:string):boolean => {
-    //     if (value.toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
-    //         return true;
-    //     } else {
-    //         return false;
-    //     }   
-    // });
-
-    // const {
-    //     value: enteredStory,
-    //     isValid: storyIsValid,
-    //     hasError: storyHasError,
-    //     areaChangeHandler: areaChangeHandler,
-    //     // inputBlurHandler: storyBlurHandler,
-    //     reset: storyReset,
-    //   } = useInput((value:string):boolean => value.trim() !== '');
-    
-
-//     let formIsValid;
-//   if (nameIsValid  && emailIsValid && storyIsValid) {
-//     formIsValid = true;
-//   }
-//   const onSubmit = (event: React.FormEvent): void =>{ 
-//     event.preventDefault();
-//     // alert(JSON.stringify(data));
-//     console.log('name:' + enteredName);
-//     console.log('email: ' + enteredEmail);
-//     console.log('story: ' + enteredStory)
-
-//     nameReset();
-//     emailReset();
-//     storyReset();
-
-//   }
-
-//   const nameInputId = nameHasError ? 'invalid' : '';
-//   const emailInputId = emailHasError ? 'invalid' : '';
-//   const storyInputId = storyHasError ? 'invalid' : '';
+    // async function handleCaptchaSubmission(token: string | null) {
+    //     // Server function to verify captcha
+    //     await verifyCaptcha(token)
+    //       .then(() => setIsVerified(true))
+    //       .catch(() => setIsVerified(false))
+    //   };
 
     return (
         <div className="container">
@@ -109,17 +52,11 @@ export default function Page () {
                 <div className="input-data">
                     <input 
                     type="text"  
-                    // {...register("name")}
-                    // onChange={nameChangeHandler}
-                    // onBlur={nameBlurHandler}
-                    // value={enteredName}
                     id='name'
                     name="Name" 
                     required />
                     <div className="underline"></div>
-                    <label htmlFor="name" className="text-[#1E1F24]">Enter your name: </label>
-                    {/* {nameHasError && <p className="text-[#b40e0e] text-xs">Name must not be empty.</p>} */}
-                   
+                    <label htmlFor="name" className="text-[#1E1F24]">Enter your name: </label>                   
                 </div>
             </div>
             <div className="form-row">
@@ -128,10 +65,6 @@ export default function Page () {
                     type="email"  
                     id='email'
                     name='Email'
-                    // {...register('email')}
-                    // onChange={emailChangeHandler}
-                    // onBlur={emailBlurHandler}
-                    // value={enteredEmail} 
                     required />
                     <div className="underline"></div>
                     <label htmlFor="email">Enter your email: 
@@ -153,10 +86,6 @@ export default function Page () {
                     // {...register('story')}  
                     rows={8}
                     cols={80}
-                    // onInput={areaChangeHandler}
-                    // onChange={storyChangeHandler}
-                    // onBlur={storyBlurHandler}
-                    // value={enteredStory}
                     required>
                     </textarea>
                     <ValidationError 
@@ -171,7 +100,7 @@ export default function Page () {
                 </div>
                 
             </div>
-            <div className='form-row'>
+            {/* <div className='form-row'>
                 <div className="input-data"> 
                     <ReCAPTCHA
                         sitekey="6LeX1CwpAAAAANPae8EO93Zoaypi7UwbI_1mzazY"
@@ -179,7 +108,7 @@ export default function Page () {
                         onChange={handleCaptchaSubmission}
                     />
                 </div>
-            </div>
+            </div> */}
             <div className='form-row submit-btn'>
             <div className="input-data">   
                 <div className="inner"></div>
